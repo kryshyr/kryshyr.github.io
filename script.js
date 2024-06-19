298ad457c34d077d1073d7f43519c451f673fe5e
kryshyr <pbsaluria@up.edu.ph>
2024-06-19 14:43:06 +0800

    Portfolio version 1

---
+++ script.js
@@ -0,0 +1,93 @@
+const body = document.body
+
+const btnTheme = document.querySelector('.fa-moon')
+const btnHamburger = document.querySelector('.fa-bars')
+
+const addThemeClass = (bodyClass, btnClass) => {
+	body.classList.add(bodyClass)
+	btnTheme.classList.add(btnClass)
+}
+
+const getBodyTheme = localStorage.getItem('portfolio-theme')
+const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
+
+addThemeClass(getBodyTheme, getBtnTheme)
+
+const isDark = () => body.classList.contains('dark')
+
+const setTheme = (bodyClass, btnClass) => {
+
+	body.classList.remove(localStorage.getItem('portfolio-theme'))
+	btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
+
+	addThemeClass(bodyClass, btnClass)
+
+	localStorage.setItem('portfolio-theme', bodyClass)
+	localStorage.setItem('portfolio-btn-theme', btnClass)
+}
+
+const toggleTheme = () =>
+	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
+
+btnTheme.addEventListener('click', toggleTheme)
+
+const displayList = () => {
+	const navUl = document.querySelector('.nav__list')
+
+	if (btnHamburger.classList.contains('fa-bars')) {
+		btnHamburger.classList.remove('fa-bars')
+		btnHamburger.classList.add('fa-times')
+		navUl.classList.add('display-nav-list')
+	} else {
+		btnHamburger.classList.remove('fa-times')
+		btnHamburger.classList.add('fa-bars')
+		navUl.classList.remove('display-nav-list')
+	}
+}
+
+btnHamburger.addEventListener('click', displayList)
+
+const scrollUp = () => {
+	const btnScrollTop = document.querySelector('.scroll-top')
+
+	if (
+		body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	) {
+		btnScrollTop.style.display = 'block'
+	} else {
+		btnScrollTop.style.display = 'none'
+	}
+}
+
+document.addEventListener('scroll', scrollUp)
+
+
+async function fetchData(type = "skills") {
+	let response
+	type === "skills" ?
+		response = await fetch("skills.json")
+		:
+		response = await fetch("./projects/projects.json")
+	const data = await response.json();
+	return data;
+}
+
+function showSkills(skills) {
+	let skillsContainer = document.getElementById("skillsContainer");
+	let skillHTML = "";
+	skills.forEach(skill => {
+		skillHTML += `
+        <div class="bar">
+			<div class="info">
+                <img class="skill-icon" src=${skill.icon} alt="skill" />
+                <span>${skill.name}</span>
+			</div>
+        </div>`
+	});
+	skillsContainer.innerHTML = skillHTML;
+}
+
+fetchData().then(data => {
+	showSkills(data);
+});
