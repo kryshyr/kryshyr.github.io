563ffda3e410fb4cba8bcc39b04c338e355a396c
kryshyr <pbsaluria@up.edu.ph>
2024-06-20 07:25:01 +0800

    UI revisions

--- script.js
+++ script.js
@@ -91,3 +91,46 @@ function showSkills(skills) {
 fetchData().then(data => {
 	showSkills(data);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('projects.json')
+        .then(response => response.json())
+        .then(projects => {
+            const projectsContainer = document.querySelector('.projects-container');
+            projects.forEach(project => {
+                const projectElement = document.createElement('div');
+                projectElement.classList.add('project');
+
+                const projectHTML = `
+                    <img src="${project.image}" alt="${project.title}">
+                    <h2>${project.title}</h2>
+                    <div class="tech-stack">
+                        ${project.techStack.map(tech => `<button class="tech-button">${tech}</button>`).join('')}
+                    </div>
+                    <div class="icon-container">
+                        ${project.links.map(link => `
+                            <a href="${link.href}" target="_blank" rel="noopener noreferrer" class="icon-link ${link.type}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                    fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="${link.type}">
+                                    ${link.type === 'github' ? `
+                                    <path
+                                        d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+                                    ` : `
+                                    <path
+                                        d="M15 7h3a5 5 0 0 1 5 5 5 5 0 0 1-5 5h-3m-6 0H6a5 5 0 0 1-5-5 5 5 0 0 1 5-5h3" />
+                                    <line x1="8" y1="12" x2="16" y2="12" />
+                                    `}
+                                </svg>
+                            </a>
+                        `).join('')}
+                    </div>
+                    <p>${project.description}</p>
+                `;
+
+                projectElement.innerHTML = projectHTML;
+                projectsContainer.appendChild(projectElement);
+            });
+        })
+        .catch(error => console.error('Error fetching projects:', error));
+});
